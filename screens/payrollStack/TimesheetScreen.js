211a584c5026d910aaf1766488658ed9ae080c9b
A5h33p <tariqsoliman2000@gmail.com>
2022-06-30 18:57:21 +1000

    Added styling to the TimesheetScreen
    Still has placeholder values and does not take data through props

--- screens/payrollStack/TimesheetScreen.js
+++ screens/payrollStack/TimesheetScreen.js
@@ -1,47 +1,62 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { FlatList, ScrollView } from "react-native-gesture-handler";
 import { ListRow, Spacing, TextStyle, ViewContainer } from "../../styles";
 import { getStatusColor } from "./TimesheetListScreen";
 
 
 export default function TimesheetScreen() {
-  return (
-    <SafeAreaView style={ViewContainer.topLeft}>
+  const statusColor = getStatusColor("APPROVED"); // Change this later
 
-      <Text style={{...TextStyle.titleNoMargin, padding: Spacing.padding.medium}}>APPROVED</Text>
-
+  const TimesheetRow = ({category, details}) => {
+    return (
       <View style={styles.rowContainer}>
+
         <View style={styles.categoryContainer}>
-          <Text style={styles.categoryText}>Category</Text>
+          <Text style={styles.categoryText}>{category}:</Text>
         </View>
 
         <View style={styles.detailContainer}>
-          <Text>Details</Text>
+          <Text style={styles.detailText}>{details}</Text>
         </View>
+
       </View>
+    );
+  }
 
-      {/* <View style={styles.categoryContainer}>
-        <Text>Category</Text>
-        <Text>Category 2</Text>
-        <Text>A Loooooooooooong Category</Text>
-      </View>
+  // Used ScrollView instead of Flatlist because there is a small number of rows in the list and the
+  // data is not stored in an array
+  return (
+    <SafeAreaView style={ViewContainer.topLeft}>
 
-      <View>
-        <Text>Details</Text>
-        <Text>Details 2</Text>
-      </View> */}
+      <Text style={[ styles.statusText, {color: statusColor} ]}>APPROVED</Text>
 
+      <ScrollView style={{width: "100%", padding: Spacing.padding.base}}>
+        <TimesheetRow category="Student" details="Name" />
+        <TimesheetRow category="Date" details="Date here" />
+        <TimesheetRow category="Start Time" details="Time here" />
+        <TimesheetRow category="Duration (min)" details={60} />
+        <TimesheetRow category="Amount Owed ($)" details={40.13} />
+        <TimesheetRow category="Additional Notes" details={"A long message that goes over" +
+      " multiple lines and just keeps going for some reason"} />
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 
 const styles = StyleSheet.create({
+  statusText: {
+    ...TextStyle.titleNoMargin,
+    marginHorizontal: Spacing.margin.medium,
+    marginTop: Spacing.margin.medium,
+  },
+
   rowContainer: {
     ...ListRow.listItem,
   },
 
   categoryContainer: {
-    width: "30%",
+    width: "40%",
   },
 
   categoryText: {
@@ -51,6 +66,7 @@ const styles = StyleSheet.create({
 
   detailContainer: {
     justifyContent: "center",
+    flex: 1,
   },
 
   detailText: {
--- styles/Spacing.js
+++ styles/Spacing.js
@@ -7,4 +7,5 @@ export const padding = {
 
 export const margin = {
   base: 10,
+  medium: 15,
 };
